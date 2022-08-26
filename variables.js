645248ca56b402664eeeb0f4c14bf5f50592bb9d
Daniel Haycraft <dhcopy1@gmail.com>
2022-08-25 19:10:21 -0700

    finished variables

--- variables.js
+++ variables.js
@@ -2,17 +2,17 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
-
+let myName = "Danny"
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
-
+let faveNum = "24"
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+let lovesCode = true
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
@@ -21,10 +21,16 @@
 */
 
 //CODE HERE
-
+if (faveNum === 13){
+  console.log('lucky 13')
+} else {
+  console.log(`not lucky 13:/ kobe bryants number! ${faveNum}`)
+}
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
-
+for (let i = 0; i < 5; i++){
+  console.log(faveNum)
+}
 //CODE HERE
