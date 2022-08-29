b5cb90817194145ce7aaee67feb93b3b34ddb27c
Daniel Haycraft <dhcopy1@gmail.com>
2022-08-29 11:59:22 -0700

    added some more to password

--- password.js
+++ password.js
@@ -1,18 +1,20 @@
-let password = "Csaldddd21!"
+
+let password = "CharlesIsacutepuppy1!"
 const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
 
 
-if (password.length >= 20 || password.length < 10 || specialChars.test(password)===false) {
-    console.log("password incorrect")
-}else if ((/[0-9]/.test(password)) && /[A-Z]/.test(password) && /[a-z]/.test(password)){
+if (password.length > 35 || password.length < 10 || specialChars.test(password) < 1) {
+    console.log("password incorrect1")
+}else if (/[0-9]/.test(password) && /[A-Z]/.test(password) && /[a-z]/.test(password)){
     console.log("password is correct today...")
 }else{
-    console.log("password incorrect")
+    console.log("password incorrect2")
 }
 
-// as soon as i realized i could do more than one conditional in side i put the incorrects on top because i feel as if they should be caught first
-// i wanted to see if there were number so i found a built in conditional /[]/.test(str)
+// i wanted to see if there were number in my password variable so i found a built in conditional /[]/.test(str)
 //you can use it with numbers and alphabet!
-// and if all else fails it goes to password in correct 
-
+// and if all else fails it goes to password incorrect 
+// i used || in the first if because if any of them were incorrect it would print incorrect 
+// and for the corrects i put the && because they all need to be true to be correct
+// /[]/.test(str) already checks for truth 
 
